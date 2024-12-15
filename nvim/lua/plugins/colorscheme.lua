2505cc11ac8ec7b0fd18daac0f390d78fdc6dc03
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2024-12-15 06:23:19 -0400

    feat(core) add new plugins
    - aerial
    - codeium
    - onedarkpro
    - eslint
    - refactoring
    - rename

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    opts = {
+      transparency = true,
+    },
+    config = function(_, opts)
+      require("onedarkpro").setup(opts)
+      vim.cmd("colorscheme onedark_vivid")
+    end,
+  },
+}
