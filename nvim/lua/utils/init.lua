a32355f80e6c6467bebaf6936f20f37ede4f3d2a
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2024-12-15 06:22:29 -0400

    fix(style) better indent on utils

--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -3,16 +3,16 @@ local LazyUtil = require("lazy.core.util")
 local M = {}
 
 function M.get_plugin(name)
-	return require("lazy.core.config").spec.plugins[name]
+  return require("lazy.core.config").spec.plugins[name]
 end
 
 function M.opts(name)
-	local plugin = M.get_plugin(name)
-	if not plugin then
-		return {}
-	end
-	local Plugin = require("lazy.core.plugin")
-	return Plugin.values(plugin, "opts", false)
+  local plugin = M.get_plugin(name)
+  if not plugin then
+    return {}
+  end
+  local Plugin = require("lazy.core.plugin")
+  return Plugin.values(plugin, "opts", false)
 end
 
 return M
