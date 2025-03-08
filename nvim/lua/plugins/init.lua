b0e9917e994c93ccc6c5760deb6d8f02c4f71c89
Alejandro Cabeza <alejandrocabeza400@gmail.com>
2025-03-08 09:08:56 -0400

    feat(core) description keymap

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,60 +1,57 @@
 return {
-  -- Lazy
-  {
-    "vague2k/vague.nvim",
-    opts = {},
-    enabled = false,
-    lazy = false,
-    priority = 100,
-    config = function(_, opts)
-      require("vague").setup(opts)
-      vim.cmd.colorscheme("vague")
-    end,
-  },
-  {
-    "direnv/direnv.vim",
-    init = function()
-      vim.g.direnv_silent_load = 1
-    end,
-  },
-  "echasnovski/mini.ai",
-  "adalessa/php-lsp-utils",
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
-  },
-  { "nvzone/volt", lazy = true },
-  { "nvzone/menu", lazy = true },
-  "tpope/vim-sleuth",
-  "nvim-lua/plenary.nvim",
-  "nvim-lua/popup.nvim",
-  "onsails/lspkind-nvim",
-  "MunifTanjim/nui.nvim",
-  "mg979/vim-visual-multi",
-  {
-    "declancm/maximize.nvim",
-    keys = {
-      { "sm", "<cmd>Maximize<cr>", desc = "Maximized Window" },
-    },
-    config = true,
-  },
-  { "Bilal2453/luvit-meta", lazy = true },
-  "jwalton512/vim-blade",
-  {
-    "tpope/vim-dispatch",
-    cmd = { "Make", "Dispatch" },
-  },
-  { "folke/lazy.nvim", version = "*" },
-  "lewis6991/impatient.nvim",
-  "tpope/vim-surround",
-  "tpope/vim-repeat",
-  "tpope/vim-rhubarb",
-  "tpope/vim-dotenv",
-  "tpope/vim-eunuch",
-  "folke/twilight.nvim",
-  "tpope/vim-surround",
-  "tpope/vim-dispatch",
+	-- Lazy
+	{
+		"vague2k/vague.nvim",
+		opts = {},
+		enabled = false,
+		lazy = false,
+		priority = 100,
+		config = function(_, opts)
+			require("vague").setup(opts)
+			vim.cmd.colorscheme("vague")
+		end,
+	},
+	{
+		"direnv/direnv.vim",
+		init = function()
+			vim.g.direnv_silent_load = 1
+		end,
+	},
+	"echasnovski/mini.ai",
+	"adalessa/php-lsp-utils",
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
+	{ "nvzone/volt", lazy = true },
+	{ "nvzone/menu", lazy = true },
+	"tpope/vim-sleuth",
+	"nvim-lua/plenary.nvim",
+	"nvim-lua/popup.nvim",
+	"onsails/lspkind-nvim",
+	"MunifTanjim/nui.nvim",
+	"mg979/vim-visual-multi",
+	{
+		"declancm/maximize.nvim",
+		config = true,
+	},
+	{ "Bilal2453/luvit-meta", lazy = true },
+	"jwalton512/vim-blade",
+	{
+		"tpope/vim-dispatch",
+		cmd = { "Make", "Dispatch" },
+	},
+	{ "folke/lazy.nvim", version = "*" },
+	"lewis6991/impatient.nvim",
+	"tpope/vim-surround",
+	"tpope/vim-repeat",
+	"tpope/vim-rhubarb",
+	"tpope/vim-dotenv",
+	"tpope/vim-eunuch",
+	"folke/twilight.nvim",
+	"tpope/vim-surround",
+	"tpope/vim-dispatch",
 }
