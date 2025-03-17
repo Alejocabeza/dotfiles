5624cc6ab603f2eb27feba80e29bf232fdea0d25
Alejandro Cabeza <alejandrocabeza400@gmail.com>
2025-03-17 00:15:56 -0400

    feat(core) update config neovim
    - add new colorschema
    - remove plugins of filename

--- nvim/lua/plugins/filename.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "b0o/incline.nvim",
-  dependencies = {
-    "echasnovski/mini.icons", -- Agrega mini.icons como dependencia
-  },
-  event = "BufReadPre",
-  priority = 1200,
-  config = function() end,
-}
